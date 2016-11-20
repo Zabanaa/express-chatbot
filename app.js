370ea3d07fd8d23f5ed703238cf47b06af22780e
= <=>
2016-11-20 14:22:03 +0000

    verify wehbook token

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ app.use(bodyParser.json())
 
 app.get('/', (req, res) => res.send("Hello Bruv"))
 
-app.get('/webhook', (req, res) => res.send("Hello Bruv"))
+app.get('/webhook', (req, res) => {
+    if (req.query['hub.verify_token'] === 'zabana') {
+        res.send(req.query['hub.challenge']);
+    } else {
+        res.send('Invalid verify token')
+    }
+})
 
 app.listen(port, () => console.log(`server listening on port ${port}`))
